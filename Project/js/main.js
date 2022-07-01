5f710eaab52a4425405d15f55e69a47b71d7a685
Blazho <aleksandar.blazhevski@outlook.com>
2022-07-01 14:28:37 +0200

    Implemented attack Player cant die

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,5 +1,7 @@
 let player;
 let enemy;
+let tier = 1;
+let first = true;
 // Set up battleground and class
 function selectClass(playerClass){
     player = new Player(playerClass);
@@ -21,13 +23,34 @@ function findOpponent(tier){
     document.querySelector(".command button").style.visibility = "hidden";
     if (!calcFirst(player.agility, enemy.agility)){
         player.health -= calcNumAttack(enemy) * calcAttack(enemy);
-        document.querySelector("#player-health").innerHTML = player.health.toString();
+        document.querySelector("#player-health").innerHTML = "Health: " + player.health.toString();
+        alert("You lost health!")
     }
 }
 function attack(){
     let attack = calcAttack(player)
     let numAttack = calcNumAttack(player);
     enemy.health -= numAttack * attack;
-    document.querySelector("#enemy-health").innerHTML = enemy.health.toString();
+    document.querySelector("#enemy-health").innerHTML = "Health: " + enemy.health.toString();
     alert("You dealt: " + numAttack + "x " + attack);
+    if (enemy.health<=0){
+        alert("You won!");
+        if (first){
+            first = false;
+        }else {
+            first = true;
+            tier++;
+        }
+        document.querySelector(".header h3").innerHTML = "Search for an opponent";
+        document.querySelector(".player").innerHTML = player.getClassAvatar();
+        document.querySelector(".command button").outerHTML = "<button onclick=\"findOpponent("+ tier +")\">Search for an opponent</button>";
+        document.querySelector(".command button").style.visibility = "visible";
+    }else {
+        player.health -= calcNumAttack(enemy) * calcAttack(enemy);
+        document.querySelector("#player-health").innerHTML = "Health: " + player.health.toString();
+        alert("You have taken damage!")
+        if (player.health <= 0){
+            alert("You lost please refresh the page to start again!");
+        }
+    }
 }
